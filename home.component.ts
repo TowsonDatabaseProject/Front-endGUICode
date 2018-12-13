bab3b8e57469c7373a18847d69f7927612fcf12d
Brennant21 <37120504+brennant21@users.noreply.github.com>
2018-12-13 18:06:43 -0500

    brennan-front-end-html-changes

---
+++ home.component.ts
@@ -0,0 +1,19 @@
+import {ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+
+
+@Component({templateUrl: 'home.component.html'})
+export class HomeComponent {
+
+
+    constructor(
+        private _activatedRoute: ActivatedRoute,
+        private _router: Router,
+    ) {}
+
+    onLoginButtonClick(): void {
+        this._router.navigate(['./login']);
+    }
+
+}
